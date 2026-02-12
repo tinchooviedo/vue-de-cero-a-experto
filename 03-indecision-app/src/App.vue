060d1595b1af2c05ee8b01ae60416e7abd2aa2d3
tinchooviedo <tinchooviedo@gmail.com>
2026-02-12 19:59:25 -0300

    Add Tailwind CSS integration and update styles
    - Updated index.html to include Tailwind CSS and set body styles.
    - Added Tailwind CSS dependencies in package.json and package-lock.json.
    - Configured Vite to use Tailwind CSS in vite.config.ts.
    - Enhanced App.vue with Tailwind classes for improved styling.
    - Created a new styles.css file to import Tailwind CSS.
    - Updated NumberCounter.vue to apply Tailwind styles to buttons and text.

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1>Hola mundo</h1>
-  <hr />
-  <NumberCounter />
+  <h1 class="text-3xl font-bold underline">Contador Wey</h1>
+  <hr class="my-4" />
+  <NumberCounter :value="5"/>
 </template>
 <script lang="ts" setup>
 import NumberCounter from './components/NumberCounter.vue';
--- 03-indecision-app/src/components/NumberCounter.vue
+++ 03-indecision-app/src/components/NumberCounter.vue
@@ -1,19 +1,28 @@
 <template>
-  <section>
-    <h3>Counter: {{ counter }}</h3>
-    <h3>Square: {{ squareCounter }}</h3>
+  <section class="mt-2">
+    <h3 class="text-xl">Counter: {{ counter }}</h3>
+    <h3 class="text-xl">Square: {{ squareCounter }}</h3>
   </section>
-  <div>
-    <button @click="counter++">+1</button>
-    <button @click="counter--">-1</button>
+  <div class="mt-4">
+    <button class="p-4 bg-blue-500 rounded hover:bg-blue-700 mr-2" @click="counter++">+1</button>
+    <button class="p-4 bg-blue-500 rounded hover:bg-blue-700 mr-2" @click="counter--">-1</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, ref } from 'vue';
 
-const counter = ref(2);
+const props = defineProps({
+  value: { type: Number, required: true },
+});
+
+const counter = ref(props.value);
 const squareCounter = computed(() => counter.value * counter.value);
 </script>
 
-<style scoped></style>
+<style lang="tailwind.css" scoped>
+/*   .button{
+    @apply pt-4 bg-blue-500 rounded hover:bg-blue-700 mr-2;
+  } */
+
+</style>

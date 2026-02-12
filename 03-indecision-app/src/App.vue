a9e47a157421c20a4b2cf47f29ad5ea4be805b64
tinchooviedo <tinchooviedo@gmail.com>
2026-02-12 12:12:46 -0300

    all files

---
+++ 03-indecision-app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hola mundo</h1>
+  <hr />
+  <NumberCounter />
+</template>
+<script lang="ts" setup>
+import NumberCounter from './components/NumberCounter.vue';
+</script>
---
+++ 03-indecision-app/src/components/NumberCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+  </section>
+  <div>
+    <button @click="counter++">+1</button>
+    <button @click="counter--">-1</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const counter = ref(2);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scoped></style>
